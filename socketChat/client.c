3460db47acb48fb65c438099bb306596add5c98f
Zizheng Wu <dustland@outlook.com>
2013-08-13 22:42:26 +0800

    copy paste from textbook

--- socketChat/client.c
+++ socketChat/client.c
@@ -1,49 +1,105 @@
-#include "stdio.h"
 #include "stdlib.h"
-#include "errno.h"
-#include "string.h"
+#include "stdio.h"
 #include "netdb.h"
 #include "sys/types.h"
-#include "netinet/in.h"
 #include "sys/socket.h"
-#define SERVPORT 3333
-#define MAXDATASIZE 1000
-int main(int argc, char const *argv[])
+#include "string.h"
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "unistd.h"
+#include "fcntl.h"
+
+#define SERVPORT 4444
+#define MAXDATASIZE 256
+#define STDIN 0
+
+int main(void)
 {
-	int sockfd, recvbytes;
+	int sockfd;
+	int recvbytes;
 	char buf[MAXDATASIZE];
-	struct hostent * host;
+	char *str;
+
+	char name[MAXDATASIZE];
+	char send_str[MAXDATASIZE];
+
 	struct sockaddr_in serv_addr;
-	if (argc < 2)
-	{
-		fprintf(stderr, "Please enter the server's hostname!\n");
-		exit(1);
-	}
-	if ((host = gethostbyname(argv[1])) == NULL)
-	{
-		herror("gethostbyname ERROR!");
-		exit(1);
-	}
+	fd_set rfd_set, wfd_set, efd_set;
+
+	struct timeval timeout;
+
+	int ret;
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
-		perror("socket create ERROR!");
+		perror("socket");
 		exit(1);
 	}
+	bzero(&serv_addr, sizeof(struct sockaddr_in));
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr = * ((struct in_addr *)host->h_addr);
-	bzero(&(serv_addr.sin_zero), 8);
+	serv_addr.sin_port = htons(SERVPORT);
+	inet_aton("127.0.0.1", &serv_addr.sin_addr);
 	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) == -1)
 	{
-		perror("connetct ERROR!");
+		perror("connect");
 		exit(1);
 	}
-	if ((recvbytes = recv(sockfd, buf, MAXDATASIZE, 0)) == -1)
+	fcntl(sockfd, F_SETFD, O_NONBLOCK);
+	printf("Let others' know your name to start chat!\n");
+	scanf("%s", name);
+	fflush(stdout);
+	send(sockfd, name, strlen(name), 0);
+
+	while(1)
 	{
-		perror("connect ERROR!");
-		exit(1);
+		FD_ZERO(&rfd_set);
+		FD_ZERO(&wfd_set);
+		FD_ZERO(&efd_set);
+		FD_SET(STDIN, &rfd_set);
+		FD_SET(sockfd, &rfd_set);
+		FD_SET(sockfd, &efd_set);
+		timeout.tv_sec = 10;
+		timeout.tv_usec = 0;
+		ret = select(sockfd + 1, &rfd_set, &wfd_set, &efd_set, &timeout);
+		if (ret == 0)
+		{
+			continue;
+		}
+		if (ret < 0)
+		{
+			perror("select error:");
+			exit(-1);
+		}
+
+		if (FD_ISSET(STDIN, &rfd_set))
+		{
+			fgets(send_str, 256, stdin);
+			send_str[strlen(send_str)-1]='\0';
+			if (strncmp("quit", send_str, 4) == 0)
+			{
+				close(sockfd);
+				exit(0);
+			}
+			send(sockfd, send_str, strlen(send_str), 0);
+		}
+
+		if (FD_ISSET(sockfd, &rfd_set))
+		{
+			recvbytes = recv(sockfd, buf, MAXDATASIZE, 0);
+			if (recvbytes == 0)
+			{
+				close(sockfd);
+				exit(0);
+			}
+			buf[recvbytes] = '\0';
+			printf("Server: %s\n", buf);
+
+			printf("%s: \n", name);
+			fflush(stdout);
+		}
+		if (FD_ISSET(sockfd, &efd_set))
+		{
+			close(sockfd);
+			exit(0);
+		}
 	}
-	buf[recvbytes] = '\0';
-	printf("receive: %s\n", buf);
-	close(sockfd);
-	return 0;
 }
